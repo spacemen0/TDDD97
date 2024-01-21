5e042f14f6429968936df83ca1c2519e93187c4d
spacemen0 <frakctures@outlook.com>
2024-01-21 02:24:19 +0100

    Refactor CSS and HTML structure

--- Lab1/client.js
+++ Lab1/client.js
@@ -1,17 +1,8 @@
 window.onload = function () {
-  // let main = document.getElementById("main");
-  // let welcome = document.getElementById("welcome");
-  // main.innerHTML = welcome.innerHTML;
+  let main = document.getElementById("main");
+  let welcome = document.getElementById("welcome");
+  main.innerHTML = welcome.innerHTML;
 };
-// var validationTimer;
-
-// function startValidation() {
-//   clearTimeout(validationTimer);
-
-//   validationTimer = setTimeout(function () {
-//     validateData();
-//   }, 1000);
-// }
 function clearValidity() {
   let password = document.getElementById("password");
   let repeatPassword = document.getElementById("repeat-psw");
@@ -26,22 +17,20 @@ function validateData() {
 
   if (password.value.length < 8) {
     password.setCustomValidity("Password must be at least 8 characters long");
-    console.log("Password must be at least 8 characters long");
     password.reportValidity();
     return false;
   } else {
     password.setCustomValidity("");
-    console.log("Password is valid");
   }
 
   if (password.value !== repeatPassword.value) {
     repeatPassword.setCustomValidity("Passwords do not match");
-    console.log("Passwords do not match");
     repeatPassword.reportValidity();
     return false;
   } else {
     repeatPassword.setCustomValidity("");
   }
+  return true;
 }
 
 function register(event) {
@@ -50,6 +39,6 @@ function register(event) {
     return false;
   }
   let form = document.getElementById("register-form");
-  form.submit();
+  form.reset();
   return true;
 }
