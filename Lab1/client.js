3f116c1da7c76f91c7bc2e85132100dc1844e54e
spacemen0 <frakctures@outlook.com>
2024-01-21 02:11:27 +0100

    correct validate

--- Lab1/client.js
+++ Lab1/client.js
@@ -1,5 +1,55 @@
 window.onload = function () {
-  let main = document.getElementById("main");
-  let welcome = document.getElementById("welcome");
-  main.innerHTML = welcome.innerHTML;
+  // let main = document.getElementById("main");
+  // let welcome = document.getElementById("welcome");
+  // main.innerHTML = welcome.innerHTML;
 };
+// var validationTimer;
+
+// function startValidation() {
+//   clearTimeout(validationTimer);
+
+//   validationTimer = setTimeout(function () {
+//     validateData();
+//   }, 1000);
+// }
+function clearValidity() {
+  let password = document.getElementById("password");
+  let repeatPassword = document.getElementById("repeat-psw");
+  password.setCustomValidity("");
+  repeatPassword.setCustomValidity("");
+}
+
+function validateData() {
+  let password = document.getElementById("password");
+  let repeatPassword = document.getElementById("repeat-psw");
+  password.setCustomValidity("");
+
+  if (password.value.length < 8) {
+    password.setCustomValidity("Password must be at least 8 characters long");
+    console.log("Password must be at least 8 characters long");
+    password.reportValidity();
+    return false;
+  } else {
+    password.setCustomValidity("");
+    console.log("Password is valid");
+  }
+
+  if (password.value !== repeatPassword.value) {
+    repeatPassword.setCustomValidity("Passwords do not match");
+    console.log("Passwords do not match");
+    repeatPassword.reportValidity();
+    return false;
+  } else {
+    repeatPassword.setCustomValidity("");
+  }
+}
+
+function register(event) {
+  event.preventDefault();
+  if (!validateData()) {
+    return false;
+  }
+  let form = document.getElementById("register-form");
+  form.submit();
+  return true;
+}
