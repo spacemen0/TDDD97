dc3ad93cde7fb1c707408ca978aee58d09c09ed8
spacemen0 <frakctures@outlook.com>
2024-02-20 01:08:34 +0100

    sign out migrate successfully

--- Lab3/twidder/static/client.js
+++ Lab3/twidder/static/client.js
@@ -20,6 +20,8 @@ function loadProfile() {
   let main = document.getElementById("main");
   let profile = document.getElementById("profile");
   main.innerHTML = profile.innerHTML;
+  loadUserInfo();
+  reloadWall();
 }
 function clearLoginValidity() {
   let password = document.getElementById("password-login");
@@ -30,6 +32,10 @@ function clearRegisterValidity() {
   let repeatPassword = document.getElementById("repeat-psw");
   password.setCustomValidity("");
   repeatPassword.setCustomValidity("");
+}
+function clearOldPasswordValidity(event) {
+  let password = document.getElementById("oldPassword");
+  password.setCustomValidity("");
 }
 
 function validateRegister() {
@@ -55,15 +61,15 @@ function validateRegister() {
 }
 
 function showMessageBox(message) {
-  var modal = document.getElementById("message-box");
-  var messageText = document.getElementById("message-text");
+  let modal = document.getElementById("message-box");
+  let messageText = document.getElementById("message-text");
 
   messageText.textContent = message;
   modal.style.display = "block";
 }
 
 function closeMessageBox() {
-  var modal = document.getElementById("message-box");
+  let modal = document.getElementById("message-box");
   modal.style.display = "none";
 }
 
@@ -89,12 +95,11 @@ function startWebsocket() {
   };
 }
 
-function profileCallback(response, token) {
+function profileCallback(response, token, load = false) {
   if (response.success) {
-    console.log(response.message);
+    showMessageBox(response.message);
     if (token !== "") localStorage.setItem("token", token);
-    loadProfile();
-    startWebsocket();
+    if (load) loadProfile();
   } else {
     let errorMessage = response.message;
     showMessageBox(errorMessage);
@@ -124,14 +129,13 @@ function register(event) {
     city: city,
     country: country,
   };
-
-  var xhr = new XMLHttpRequest();
+  let xhr = new XMLHttpRequest();
   xhr.open("POST", server_url + "/sign_up", true);
   xhr.setRequestHeader("Content-Type", "application/json");
 
   xhr.onreadystatechange = function () {
     if (xhr.readyState == 4) {
-      var responseData = JSON.parse(xhr.responseText);
+      let responseData = JSON.parse(xhr.responseText);
       console.log(responseData);
       const authorizationHeader = xhr.getResponseHeader("Authorization");
       let token = "";
@@ -141,10 +145,9 @@ function register(event) {
       profileCallback(responseData, token);
     }
   };
-  var requestBody = JSON.stringify(dataObject);
+  let requestBody = JSON.stringify(dataObject);
   xhr.send(requestBody);
 }
-
 function validateLogin() {
   let password = document.getElementById("password-login");
   if (password.value.length < 8) {
@@ -164,36 +167,234 @@ function login(event) {
 
   let email = document.getElementById("email-login").value;
   let password = document.getElementById("password-login").value;
+
   let dataObject = {
     email: email,
     password: password,
   };
 
-  var xhr = new XMLHttpRequest();
+  let xhr = new XMLHttpRequest();
   xhr.open("POST", server_url + "/sign_in", true);
   xhr.setRequestHeader("Content-Type", "application/json");
 
   xhr.onreadystatechange = function () {
     if (xhr.readyState == 4) {
-      var responseData = JSON.parse(xhr.responseText);
+      let responseData = JSON.parse(xhr.responseText);
       console.log(responseData);
       const authorizationHeader = xhr.getResponseHeader("Authorization");
       let token = "";
       if (authorizationHeader && authorizationHeader.startsWith("Bearer ")) {
         token = authorizationHeader.slice(7);
       }
-      profileCallback(responseData, token);
+      profileCallback(responseData, token, true);
     }
   };
-  var requestBody = JSON.stringify(dataObject);
+  let requestBody = JSON.stringify(dataObject);
   xhr.send(requestBody);
 }
 
 function showTab(tabId) {
-  var tabs = document.getElementsByClassName("tabs");
-  for (var i = 0; i < tabs.length; i++) {
+  let tabs = document.getElementsByClassName("tabs");
+  for (let i = 0; i < tabs.length; i++) {
     tabs[i].classList.remove("active-tab");
   }
 
   document.getElementById(tabId).classList.add("active-tab");
+}
+
+function validateOldPassword() {
+  let password = document.getElementById("oldPassword");
+  if (password.value.length < 8) {
+    password.setCustomValidity("Password must be at least 8 characters long");
+    password.reportValidity();
+    return false;
+  } else {
+    password.setCustomValidity("");
+  }
+  return true;
+}
+
+function changePassword(event) {
+  event.preventDefault();
+  if (!validateOldPassword() || !validateRegister()) {
+    return false;
+  }
+  let token = JSON.parse(localStorage.getItem("token"));
+  let oldPassword = document.getElementById("oldPassword").value;
+  let newPassword = document.getElementById("password").value;
+
+  let changeResult = serverstub.changePassword(token, oldPassword, newPassword);
+  displayMessage(changeResult.message, "change");
+}
+
+function signOutCallback(response) {
+  if (response.success) {
+    showMessageBox(response.message);
+    localStorage.removeItem("token");
+    loadWelcome();
+  } else {
+    let errorMessage = response.message;
+    showMessageBox(errorMessage);
+  }
+}
+function signOut(event) {
+  event.preventDefault();
+
+  let xhr = new XMLHttpRequest();
+  xhr.open("DELETE", server_url + "/sign_out", true);
+  let token = localStorage.getItem("token");
+  xhr.setRequestHeader("Authorization", "Bearer " + token);
+
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState == 4) {
+      let responseData = JSON.parse(xhr.responseText);
+      console.log(responseData);
+      signOutCallback(responseData);
+    }
+  };
+  xhr.send();
+}
+
+function displayMessage(message, method) {
+  let messageElement = document.getElementById(method + "-message");
+  messageElement.textContent = message;
+}
+
+function loadUserInfo() {
+  let token = localStorage.getItem("token");
+  let userInfo = serverstub.getUserDataByToken(token);
+  if (userInfo.success) {
+    let user = userInfo.data;
+    document.getElementById("user-info").innerHTML =
+      "<strong>First Name:</strong> " +
+      user.firstname +
+      "<br>" +
+      "<strong>Last Name:</strong> " +
+      user.familyname +
+      "<br>" +
+      "<strong>Email:</strong> " +
+      user.email +
+      "<br>" +
+      "<strong>Gender:</strong> " +
+      user.gender +
+      "<br>" +
+      "<strong>City:</strong> " +
+      user.city +
+      "<br>" +
+      "<strong>Country:</strong> " +
+      user.country;
+  }
+}
+
+function loadWall() {
+  let token = JSON.parse(localStorage.getItem("token"));
+  let wallInfo = serverstub.getUserMessagesByToken(token);
+  if (wallInfo.success) {
+    let wall = wallInfo.data;
+    let wallList = document.getElementById("wall");
+    wallList.innerHTML = "";
+    wall.forEach(function (message) {
+      wallList.innerHTML +=
+        "<li><p>" + message.writer + ": " + message.content + "</p></li>";
+    });
+  }
+}
+
+function postMessage() {
+  let message = document.getElementById("post-message").value.trim();
+  if (message !== "") {
+    let token = JSON.parse(localStorage.getItem("token"));
+    let postResult = serverstub.postMessage(token, message);
+    if (postResult.success) {
+      loadWall();
+      document.getElementById("post-message").value = "";
+    } else {
+      showMessageBox(postResult.message);
+    }
+  } else {
+    showMessageBox("Please enter a message.");
+  }
+}
+
+function reloadWall() {
+  loadWall();
+}
+
+function browseUser() {
+  let userEmail = document.getElementById("searching-email").value;
+  let token = JSON.parse(localStorage.getItem("token"));
+  let userData = serverstub.getUserDataByEmail(token, userEmail);
+
+  if (userData.success) {
+    displayUser(userData.data);
+  } else {
+    document.getElementById("search-feedback").textContent = userData.message;
+    clearBrowseData();
+  }
+}
+
+function displayUser(user) {
+  document.getElementById("search-feedback").innerHTML =
+    "<strong>First Name:</strong> " +
+    user.firstname +
+    "<br>" +
+    "<strong>Last Name:</strong> " +
+    user.familyname +
+    "<br>" +
+    "<strong>Email:</strong> " +
+    user.email +
+    "<br>" +
+    "<strong>Gender:</strong> " +
+    user.gender +
+    "<br>" +
+    "<strong>City:</strong> " +
+    user.city +
+    "<br>" +
+    "<strong>Country:</strong> " +
+    user.country;
+  let token = JSON.parse(localStorage.getItem("token"));
+  let searchResult = serverstub.getUserMessagesByEmail(token, user.email);
+  if (searchResult.success) {
+    let messages = searchResult.data;
+
+    let postMessageForm = `
+    <h3>Post a Message:</h3>
+    <textarea id="post-notes" rows="4" cols="20"></textarea><br>
+    <button onclick="postOthersMessage()">Post</button>
+    <button onclick="reloadPost()">Reload</button>
+`;
+    document.getElementById("search-feedback").innerHTML += postMessageForm;
+
+    let wallHTML = `<div id="wall-wrapper"><h3>Wall Messages:</h3><ul id="wall">`;
+    messages.forEach(function (message) {
+      wallHTML +=
+        "<li><p>" + message.writer + ": " + message.content + "</p></li>";
+    });
+    wallHTML += "</ul></div>";
+    document.getElementById("otherwall").innerHTML = wallHTML;
+  } else {
+    document.getElementById("search-feedback").innerHTML +=
+      "<p>No messages found on this user's wall.</p>";
+  }
+}
+
+function postOthersMessage() {
+  let token = JSON.parse(localStorage.getItem("token"));
+  let message = document.getElementById("post-notes").value;
+  if (message !== "") {
+    let email = document.getElementById("searching-email").value;
+    let posted = serverstub.postMessage(token, message, email);
+
+    if (posted.success) {
+      reloadPost();
+    } else {
+      showMessageBox(postResult.message);
+    }
+  } else {
+    showMessageBox("Please enter a message.");
+  }
+}
+
+function reloadPost() {
+  browseUser();
 }
---
+++ Lab3/twidder/static/clientOriginal.js
@@ -0,0 +1,199 @@
+const server_url = "http://127.0.0.1:5000";
+const socket_url = "ws://127.0.0.1:5000/sock";
+let socket;
+window.onload = function () {
+  if (localStorage.getItem("token")) {
+    loadProfile();
+    startWebsocket();
+  } else {
+    loadWelcome();
+  }
+};
+
+function loadWelcome() {
+  let main = document.getElementById("main");
+  let welcome = document.getElementById("welcome");
+  main.innerHTML = welcome.innerHTML;
+}
+
+function loadProfile() {
+  let main = document.getElementById("main");
+  let profile = document.getElementById("profile");
+  main.innerHTML = profile.innerHTML;
+}
+function clearLoginValidity() {
+  let password = document.getElementById("password-login");
+  password.setCustomValidity("");
+}
+function clearRegisterValidity() {
+  let password = document.getElementById("password");
+  let repeatPassword = document.getElementById("repeat-psw");
+  password.setCustomValidity("");
+  repeatPassword.setCustomValidity("");
+}
+
+function validateRegister() {
+  let password = document.getElementById("password");
+  let repeatPassword = document.getElementById("repeat-psw");
+
+  if (password.value.length < 8) {
+    password.setCustomValidity("Password must be at least 8 characters long");
+    password.reportValidity();
+    return false;
+  } else {
+    password.setCustomValidity("");
+  }
+
+  if (password.value !== repeatPassword.value) {
+    repeatPassword.setCustomValidity("Passwords do not match");
+    repeatPassword.reportValidity();
+    return false;
+  } else {
+    repeatPassword.setCustomValidity("");
+  }
+  return true;
+}
+
+function showMessageBox(message) {
+  var modal = document.getElementById("message-box");
+  var messageText = document.getElementById("message-text");
+
+  messageText.textContent = message;
+  modal.style.display = "block";
+}
+
+function closeMessageBox() {
+  var modal = document.getElementById("message-box");
+  modal.style.display = "none";
+}
+
+function ManuallyLogOut() {
+  localStorage.removeItem("token");
+  loadWelcome();
+  if (socket.readyState == 1) socket.close();
+}
+
+function startWebsocket() {
+  if (!localStorage.getItem("token")) return;
+  socket = new WebSocket(socket_url);
+  socket.addEventListener("message", (ev) => {
+    if (ev.data === "Log Out") ManuallyLogOut();
+  });
+  socket.onopen = (event) => {
+    intervalId = setInterval(() => {
+      if (!localStorage.getItem("token")) {
+        clearInterval(intervalId);
+      }
+      socket.send(localStorage.getItem("token"));
+    }, 500);
+  };
+}
+
+function profileCallback(response, token) {
+  if (response.success) {
+    console.log(response.message);
+    if (token !== "") localStorage.setItem("token", token);
+    loadProfile();
+    startWebsocket();
+  } else {
+    let errorMessage = response.message;
+    showMessageBox(errorMessage);
+  }
+}
+
+function register(event) {
+  event.preventDefault();
+  if (!validateRegister()) {
+    return false;
+  }
+
+  let email = document.getElementById("email").value;
+  let password = document.getElementById("password").value;
+  let firstName = document.getElementById("first-name").value;
+  let familyName = document.getElementById("family-name").value;
+  let gender = document.getElementById("gender").value;
+  let city = document.getElementById("city").value;
+  let country = document.getElementById("country").value;
+
+  let dataObject = {
+    email: email,
+    password: password,
+    firstname: firstName,
+    familyname: familyName,
+    gender: gender,
+    city: city,
+    country: country,
+  };
+
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", server_url + "/sign_up", true);
+  xhr.setRequestHeader("Content-Type", "application/json");
+
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState == 4) {
+      var responseData = JSON.parse(xhr.responseText);
+      console.log(responseData);
+      const authorizationHeader = xhr.getResponseHeader("Authorization");
+      let token = "";
+      if (authorizationHeader && authorizationHeader.startsWith("Bearer ")) {
+        token = authorizationHeader.slice(7);
+      }
+      profileCallback(responseData, token);
+    }
+  };
+  var requestBody = JSON.stringify(dataObject);
+  xhr.send(requestBody);
+}
+
+function validateLogin() {
+  let password = document.getElementById("password-login");
+  if (password.value.length < 8) {
+    password.setCustomValidity("Password must be at least 8 characters long");
+    password.reportValidity();
+    return false;
+  } else {
+    password.setCustomValidity("");
+  }
+  return true;
+}
+function login(event) {
+  event.preventDefault();
+  if (!validateLogin()) {
+    return false;
+  }
+
+  let email = document.getElementById("email-login").value;
+  let password = document.getElementById("password-login").value;
+  let dataObject = {
+    email: email,
+    password: password,
+  };
+
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", server_url + "/sign_in", true);
+  xhr.setRequestHeader("Content-Type", "application/json");
+
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState == 4) {
+      var responseData = JSON.parse(xhr.responseText);
+      console.log(responseData);
+      const authorizationHeader = xhr.getResponseHeader("Authorization");
+      let token = "";
+      if (authorizationHeader && authorizationHeader.startsWith("Bearer ")) {
+        token = authorizationHeader.slice(7);
+      }
+      profileCallback(responseData, token);
+    }
+  };
+  var requestBody = JSON.stringify(dataObject);
+  xhr.send(requestBody);
+}
+
+function showTab(tabId) {
+  var tabs = document.getElementsByClassName("tabs");
+  for (var i = 0; i < tabs.length; i++) {
+    tabs[i].classList.remove("active-tab");
+  }
+
+  document.getElementById(tabId).classList.add("active-tab");
+}
